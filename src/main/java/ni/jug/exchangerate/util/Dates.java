0ed20a36c9262a184a13572780fc8b0aa6534097
Armando Alaniz <arjoalaniz@gmail.com>
2019-03-20 23:00:41 -0600

    Calcular los dias del mes actual

--- src/main/java/ni/jug/exchangerate/util/Dates.java
+++ src/main/java/ni/jug/exchangerate/util/Dates.java
@@ -1,6 +1,7 @@
 package ni.jug.exchangerate.util;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  *
@@ -13,6 +14,12 @@ public final class Dates {
 
     public static int toIsoNumericFormat(LocalDate date) {
         return (date.getYear()*100 + date.getMonthValue())*100 + date.getDayOfMonth();
+    }
+
+    public static long daysForCurrentPeriod() {
+        LocalDate firstDay = LocalDate.now().withDayOfMonth(1);
+        LocalDate firstDayNextMonth = firstDay.plusMonths(1);
+        return ChronoUnit.DAYS.between(firstDay, firstDayNextMonth);
     }
 
 }
