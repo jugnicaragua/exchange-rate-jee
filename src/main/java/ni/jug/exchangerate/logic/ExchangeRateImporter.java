48588bb4a479127e36be5fce5776a0417fb7be30
Armando Alaniz <arjoalaniz@gmail.com>
2019-03-28 10:41:18 -0600

    Crear tarea para importar tasas de cambio del BCN desde el 2012

--- src/main/java/ni/jug/exchangerate/logic/ExchangeRateImporter.java
+++ src/main/java/ni/jug/exchangerate/logic/ExchangeRateImporter.java
@@ -4,8 +4,15 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,6 +23,7 @@ import ni.jug.exchangerate.model.CentralBankExchangeRate;
 import ni.jug.exchangerate.model.CommercialBankExchangeRate;
 import ni.jug.exchangerate.model.Currency;
 import ni.jug.exchangerate.util.Dates;
+import ni.jug.ncb.exchangerate.ExchangeRateBCNClient;
 import ni.jug.ncb.exchangerate.ExchangeRateScraper;
 import ni.jug.ncb.exchangerate.MonthlyExchangeRate;
 
@@ -24,12 +32,16 @@ import ni.jug.ncb.exchangerate.MonthlyExchangeRate;
  * @author aalaniz
  */
 @Singleton
+@Startup
 public class ExchangeRateImporter {
 
     private static final String DOLLAR_ISO_STRING_CODE = "USD";
 
     @PersistenceContext
     EntityManager em;
+
+    @Resource
+    TimerService timerService;
 
     @Inject
     CurrencyProvider currencyProvider;
@@ -42,6 +54,29 @@ public class ExchangeRateImporter {
 
     private Currency getDollar() {
         return currencyProvider.getCurrencyByIsoStringCode(DOLLAR_ISO_STRING_CODE);
+    }
+
+    @PostConstruct
+    public void onInit() {
+        timerService.createSingleActionTimer(20000, new TimerConfig());
+    }
+
+    @Timeout
+    public void importInitialCentralBankExchangeRate(Timer timer) {
+        Currency dollar = getDollar();
+        LocalDate processDate = LocalDate.of(ExchangeRateBCNClient.MINIMUM_YEAR, 1, 1);
+        LocalDate endDate = LocalDate.now().withDayOfMonth(1);
+
+        while (processDate.isBefore(endDate)) {
+            MonthlyExchangeRate monthlyExchangeRate = new ExchangeRateScraper().getMonthlyExchangeRate(processDate);
+            for (Map.Entry<LocalDate, BigDecimal> exchangeRateByDate : monthlyExchangeRate.getMonthlyExchangeRate().entrySet()) {
+                CentralBankExchangeRate exchangeRate = new CentralBankExchangeRate(dollar, exchangeRateByDate.getKey(),
+                        exchangeRateByDate.getValue());
+                em.merge(exchangeRate);
+            }
+
+            processDate = processDate.plusMonths(1);
+        }
     }
 
     @Schedule(hour = "*", minute = "*/15", persistent = false)
